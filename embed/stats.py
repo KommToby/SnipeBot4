5688cf39e2f5a7a1e8eda59fc953ff522581eaab
Toby <kommandertoby@gmail.com>
2022-07-28 01:42:10 +0100

    Mod conversion to stats

--- embed/stats.py
+++ embed/stats.py
@@ -1,4 +1,36 @@
-import interactions
+import interactions, random
+
+async def update_decode(modint, value, modarray, mod):
+    modarray.append(mod)
+    modint = modint - value
+    return modint, modarray
+
+async def decode_mods_to_array(modint): # converts mod integer back into array of mods
+    modarray = []
+    moddict = {
+        16384: "PF",
+        8192: "Relax2",
+        4096: "SO",
+        2048: "Autoplay",
+        1024: "FL",
+        512: "NC",
+        256: "HT",
+        128: "RX",
+        64: "DT",
+        32: "SD",
+        16: "HR",
+        8: "HD",
+        4: "TD",
+        2: "EZ",
+        1: "NF"
+    }
+    for key in moddict:
+        if modint >= key:
+            if (modint-key)>=0:
+                modint, modarray = await update_decode(modint, key, modarray, moddict[key])
+    if modint != 1:
+        print("An error occured when decoding mod array")
+    return modarray
 
 async def frequency_check(array):
     title = ""
@@ -41,12 +73,22 @@ async def create_stats_embed(user, user_score_data, top_ten_artists, scores):
     best_song, best_song_freq = await frequency_check(user_score_data['songs'])
     best_guest, best_guest_freq = await frequency_check(user_score_data['guests'])
     best_mapper, best_mapper_freq = await frequency_check(user_score_data['mappers'])
+    best_mod, best_mod_freq = await frequency_check(user_score_data['mods'])
+    best_mod_array = await decode_mods_to_array(best_mod)
+    best_mod_string = ""
+    for mod in reversed(best_mod_array):
+        best_mod_string += f"{mod}"
+    best_mod_string = best_mod_string
 
     embed.description = artist_string
 
     embed.add_field(name=f"Average SR of all your stored plays: `{round((sum(user_score_data['stars'])/len(user_score_data['stars'])), 2)}` ({len(scores)} stored plays!)\nAverage length of stored plays: `{average_map_length_string}`\nAverage BPM of stored plays: `{round((sum(user_score_data['bpm'])/len(user_score_data['bpm'])), 2)}`",
-                    value=f"**Most played song: `{best_song}` ({best_song_freq} instances)**\n**Favourite guest difficulty mapper: `{best_guest}` ({best_guest_freq} instances)**\n**Favourite mapper: `{best_mapper}` ({best_mapper_freq} instances)**")
+                    value=f"**Most played song: `{best_song}` ({best_song_freq} instances)**\n**Favourite guest difficulty mapper: `{best_guest}` ({best_guest_freq} instances)**\n**Favourite mapper: `{best_mapper}` ({best_mapper_freq} instances)**\n**Favourite mod combination: `{best_mod_string}` ({best_mod_freq} instances)**")
 
-    embed.set_footer(text="Fact of the day: Did you know that Shii is the mapper of Lagtrain?", icon_url="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Blue_question_mark_icon.svg/1200px-Blue_question_mark_icon.svg.png")
+    ## I got bullied into this
+    facts_of_the_day = ["Did you know that Shii is the mapper of Lagtrain?",
+                        "Did you know that this fact of the day is new?"]
+
+    embed.set_footer(text=f"Fact of the day: {random.choice(facts_of_the_day)}", icon_url="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Blue_question_mark_icon.svg/1200px-Blue_question_mark_icon.svg.png")
 
     return embed
