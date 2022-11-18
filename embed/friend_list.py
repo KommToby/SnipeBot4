bb4d063a5b6cfd33f7a3d4288121c51332b1d1a2
Toby <kommandertoby@gmail.com>
2022-11-18 08:02:19 +0000

    put friend list in a code block to prevent autoformat

--- embed/friend_list.py
+++ embed/friend_list.py
@@ -13,6 +13,8 @@ async def create_friend_list_embed(user: UserData, friends):
         friend_string = ""
         for friend in friends:
             friend_string += f"{friend[2]}\n"
+        # put every name in a code block
+        friend_string = f"```{friend_string}```"
         embed.add_field(name=f"User has {len(friends)} friends:", value=friend_string, inline=False)
     else:
         embed.add_field(name=f"User has 0 friends", value="No friends found!", inline=False)
