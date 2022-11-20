a705d6536704922e9676e6da2b97efd4f5960e0e
Toby <kommandertoby@gmail.com>
2022-11-20 10:24:51 +0000

    fixed small syntax

--- embed/snipelist.py
+++ embed/snipelist.py
@@ -4,7 +4,7 @@ from data_types.osu import UserData
 
 async def create_snipelist_embed(friend_username: str, beatmaps: list, links: list):
     embed = interactions.Embed(
-        title=f"10 random maps the main user should snipe {friend_username} back on",
+        title=f"10 random maps the {friend_username} should snipe the main user back on",
         color=16753920,
     )
     # shuffle the beatmaps and links so that they are in a random order but the same order
